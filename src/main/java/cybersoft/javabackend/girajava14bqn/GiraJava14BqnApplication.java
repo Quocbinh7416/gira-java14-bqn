a5cdaa010166117f9b31ddefb2ee66cd01059069
Nguyen Quoc Binh <85874078+Quocbinh7416@users.noreply.github.com>
2021-12-15 18:37:22 +0700

    Initial commit

---
+++ src/main/java/cybersoft/javabackend/girajava14bqn/GiraJava14BqnApplication.java
@@ -0,0 +1,13 @@
+package cybersoft.javabackend.girajava14bqn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiraJava14BqnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiraJava14BqnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cybersoft/javabackend/girajava14bqn/GiraJava14BqnApplicationTests.java
@@ -0,0 +1,13 @@
+package cybersoft.javabackend.girajava14bqn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiraJava14BqnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
