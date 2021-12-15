f73f585eeae918c6361b604537a8d4a7c8587b4c
Nguyen Quoc Binh <85874078+Quocbinh7416@users.noreply.github.com>
2021-12-15 19:01:19 +0700

    fix open api configuration
    OpenApiConfiguration:
    application.yml: add api-docs path

---
+++ src/main/java/cybersoft/javabackend/girajava14bqn/role/model/Role.java
@@ -0,0 +1,18 @@
+package cybersoft.javabackend.girajava14bqn.role.model;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class Role {
+	private String id;
+	private String name;
+}
+
